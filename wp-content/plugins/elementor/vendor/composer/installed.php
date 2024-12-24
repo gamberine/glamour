41cb01fd2f14b72f672cf75ae7b487633b8958c9
gamberine <gamberine@gmail.com>
2024-12-24 00:59:25 -0300

    atualização elementor e tema store front

--- wp-content/plugins/elementor/vendor/composer/installed.php
+++ wp-content/plugins/elementor/vendor/composer/installed.php
@@ -1,9 +1,9 @@
 <?php return array(
     'root' => array(
         'name' => 'elementor/elementor',
-        'pretty_version' => '3.25.x-dev',
-        'version' => '3.25.9999999.9999999-dev',
-        'reference' => 'e5d597180adac6eab93a7e3751f443749ed30fbe',
+        'pretty_version' => '3.26.x-dev',
+        'version' => '3.26.9999999.9999999-dev',
+        'reference' => '71f278ef91ed7a04c5a0db61f8959fee7a51b1f3',
         'type' => 'project',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -11,9 +11,9 @@
     ),
     'versions' => array(
         'elementor/elementor' => array(
-            'pretty_version' => '3.25.x-dev',
-            'version' => '3.25.9999999.9999999-dev',
-            'reference' => 'e5d597180adac6eab93a7e3751f443749ed30fbe',
+            'pretty_version' => '3.26.x-dev',
+            'version' => '3.26.9999999.9999999-dev',
+            'reference' => '71f278ef91ed7a04c5a0db61f8959fee7a51b1f3',
             'type' => 'project',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
